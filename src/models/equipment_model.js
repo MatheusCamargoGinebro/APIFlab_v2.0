cd8c1ba3ee48ed1b0f48af019b3ecd6d800e0992
Camarg0vs <matheus.ginebro@aluno.ifsp.edu.br>
2025-10-08 10:39:30 -0300

    Adiciona funções de model para equipamentos e exporta as mesmas

--- src/models/equipment_model.js
+++ src/models/equipment_model.js
@@ -6,7 +6,17 @@
     O======================================================O
 
     Lista de funções:  
-    - []
+    - [] registerEquipments
+    - [] getEquipmentById
+    - [] deleteEquipment
+    - [] ListLabEquipments
+    - [] getEquipmentsBySession
+    - [] editEquipmentName
+    - [] editEquipmentQuantity
+    - [] editEquipmentQuality
+    - [] editEquipmentDescription
+    - [] editEquipmentAdmin
+    - [] editEquipmentImage
 */
 
 // O========================================================================================O
@@ -16,9 +26,74 @@ const connection = require("../utils/connection");
 
 // O========================================================================================O
 
+// Função para
+const registerEquipments = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const getEquipmentById = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const deleteEquipment = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const ListLabEquipments = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const getEquipmentsBySession = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const editEquipmentName = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const editEquipmentQuantity = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const editEquipmentQuality = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const editEquipmentDescription = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const editEquipmentAdmin = async () => {};
+
+// O========================================================================================O
+
+// Função para
+const editEquipmentImage = async () => {};
+
 // O========================================================================================O
 
 // Exportando as funções:
-module.exports = {};
+module.exports = {
+	registerEquipments,
+	getEquipmentById,
+	deleteEquipment,
+	ListLabEquipments,
+	getEquipmentsBySession,
+	editEquipmentName,
+	editEquipmentQuantity,
+	editEquipmentQuality,
+	editEquipmentDescription,
+	editEquipmentAdmin,
+	editEquipmentImage,
+};
 
 // O========================================================================================O
