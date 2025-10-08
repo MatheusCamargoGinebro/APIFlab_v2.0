7b7ea4b62c08b2159f762a691e36e993e7d94277
Camarg0vs <matheus.ginebro@aluno.ifsp.edu.br>
2025-10-08 10:43:34 -0300

    Atualiza comentários das funções no modelo de equipamentos para maior clareza e consistência.

--- src/models/equipment_model.js
+++ src/models/equipment_model.js
@@ -26,57 +26,57 @@ const connection = require("../utils/connection");
 
 // O========================================================================================O
 
-// Função para
+// Função para registrar equipamentos:
 const registerEquipments = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para pegar equipamento pelo ID:
 const getEquipmentById = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para deletar equipamento:
 const deleteEquipment = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para listar equipamentos do laboratório:
 const ListLabEquipments = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para pegar equipamentos pela sessão:
 const getEquipmentsBySession = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para editar nome do equipamento:
 const editEquipmentName = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para editar quantidade do equipamento:
 const editEquipmentQuantity = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para editar qualidade do equipamento:
 const editEquipmentQuality = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para editar descrição do equipamento:
 const editEquipmentDescription = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para editar nível de administração do equipamento:
 const editEquipmentAdmin = async () => {};
 
 // O========================================================================================O
 
-// Função para
+// Função para editar imagem do equipamento:
 const editEquipmentImage = async () => {};
 
 // O========================================================================================O
