9df0ef98d9a4c4e7e6dd3b6d5ebaf2499f47e974
Camarg0vs <matheus.ginebro@aluno.ifsp.edu.br>
2025-10-12 11:55:11 -0300

    melhora a resposta de solver

--- src/utils/solver.js
+++ src/utils/solver.js
@@ -5,8 +5,16 @@
 */
 // O========================================================================================O
 
-const solver = (__request, response) => {
-	return response.status(200).json({ status: true, whoAmI: "APIFLab_v2.0" });
+// O========================================================================================O
+
+const solver = (request, response) => {
+	const { challenge } = request.params;
+
+	console.log(challenge);
+
+	return response
+		.status(200)
+		.json({ status: true, whoAmI: "APIFLab_v2.0", solveChallenge: challenge });
 };
 
 module.exports = solver;
